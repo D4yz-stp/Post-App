ff69b678c698dbfa173f819b027a6e0e33d9a3ec
Divaldo Dias <164824282+D4yz-stp@users.noreply.github.com>
2025-05-09 00:56:39 +0100

    Nothing

--- lib/screens/home_screen.dart
+++ lib/screens/home_screen.dart
@@ -1,6 +1,6 @@
 
 import 'package:flutter/material.dart';
-
+import '../models/post.dart';
 class HomeScreen extends StatefulWidget {
   const HomeScreen({Key? key}) : super(key: key);
 
@@ -9,5 +9,39 @@ class HomeScreen extends StatefulWidget {
 }
 
 class _HomeScreenState extends State<HomeScreen> {
-  
+  final List<Post> posts = List.generate(
+    10,
+    (index) => Post(id: index + 1, title: 'Post ${index + 1}'),
+  );
+
+  int _currentIndex = 0;
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('Posts'),
+        backgroundColor: Theme.of(context).colorScheme.primary,
+        foregroundColor: Colors.white,
+      ),
+      body: _buildBody(),
+    );
+  }
+
+  Widget _buildBody() {
+    switch (_currentIndex) {
+      case 0:
+        return _buildPostsList();
+    }
+  }
+
+  Widget _buildPostsList() {
+    return ListView.builder(
+      padding: const EdgeInsets.all(8.0),
+      itemCount: posts.length,
+      itemBuilder: (context, index) {
+        return // I need to implement a class of PostCard
+      },
+    );
+  }
 }
