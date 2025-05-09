075cf9a8fec933ed25ac6d60313f0cf309a53daa
Divaldo Dias <164824282+D4yz-stp@users.noreply.github.com>
2025-05-09 02:09:16 +0100

    FINISHED! Now i just need to put it clean code and beautifull

---
+++ lib/models/comment.dart
@@ -0,0 +1,9 @@
+class Comment {
+  final int postId;
+  final int id;
+  final String name;
+  final String email;
+  final String text;
+
+  Comment({required this.postId, required this.id,required this.name,required this.email,required this.text});
+}
--- lib/screens/detail_screen.dart
+++ lib/screens/detail_screen.dart
@@ -1,8 +1,8 @@
 import 'package:flutter/material.dart';
+import 'package:teste/models/comment.dart';
 import '../models/post.dart';
- 
-
-// simple detail screen widget just to see if its working 
+import 'package:http/http.dart' as http;
+import 'dart:convert';
 class DetailScreen extends StatelessWidget {
   final Post post;
 
@@ -16,9 +16,42 @@ class DetailScreen extends StatelessWidget {
         backgroundColor: Theme.of(context).colorScheme.primary,
         foregroundColor: Colors.white,
       ),
-      body: Center(
-        child: Text('Details for ${post.title} will be displayed here'),
+      body: FutureBuilder<List<dynamic>>(
+        future: fetchComments(post.id), 
+        builder: (context, snapshot) {
+          if (snapshot.connectionState == ConnectionState.waiting) {
+            return const Center(child: CircularProgressIndicator());
+          } else if (snapshot.hasError) {
+            return Center(child: Text('Error: ${snapshot.error}'));
+          } else if (!snapshot.hasData || snapshot.data!.isEmpty) {
+            return const Center(child: Text('No comments found.'));
+          } else {
+            // Se os dados forem carregados corretamente, mostramos os comentários
+            List<dynamic> comments = snapshot.data!;
+            return ListView.builder(
+              itemCount: comments.length,
+              itemBuilder: (context, index) {
+                return ListTile(
+                  title: Text(comments[index]['name']),
+                  subtitle: Text(comments[index]['body']), // A chave 'body' contém o texto do comentário
+                );
+              },
+            );
+          }
+        },
       ),
     );
+  }
+}
+
+
+Future<List<dynamic>> fetchComments(int postId) async {
+  final response = await http.get(Uri.parse('https://jsonplaceholder.typicode.com/comments?postId=$postId'));
+
+  if (response.statusCode == 200) {
+    List<dynamic> data = json.decode(response.body);
+    return data;
+  } else {
+    throw Exception('Failed to load comments');
   }
 }
